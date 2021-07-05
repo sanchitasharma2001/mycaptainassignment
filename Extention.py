2e7b8f6bab8c379300eb110c80ec949c70c5475b
Sanchita Sharma <65801512+sanchitasharma2001@users.noreply.github.com>
2021-07-06 02:46:57 +0530

    Add files via upload

---
+++ Extention.py
@@ -0,0 +1,4 @@
+#This program will give extention of the file that you input, as the output...
+file_name=input("Write a file_name:")
+file_ext=file_name.split(".")
+print("The exetention is:"+ repr (file_ext[-1]))
